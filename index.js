3bd3a2a6ce20d518b682c2b9df3f4c8257f30ed7
Carl-Johan Blomqvist <carl-johan@findsourcing.com>
2020-06-12 09:56:56 +0200

    feat/test: Add support for converting size ranges. This includes support for A) handling "in-between" sizes, B) handling inches as numbers with automatic conversion back and forth C) Initial kids sizes support

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const unisex = {
 	br: [33, 34, 34, 35, 36, 36, 37, 37, 38, 39, 40, 41, 42, 43, 45],
 	cm: [23, 23, 24, 24, 24, 25, 25, 25, 25, 25.7, 26, 26.7, 27.3, 28, 28.6],
 	in: [
-		'9',
-		'9 1/8',
-		'9 1/4',
-		'9 3/8',
-		'9 1/2',
-		'9 5/8',
-		'9 3/4',
-		'9 7/8',
-		'10',
-		'10 1/8',
-		'10 1/4',
-		'10 1/2',
-		'10 3/4',
-		'11',
-		'11 1/4'
+		9, // 9
+		9.125, // 9 1/8
+		9.25, // 9 1/4
+		9.375, // 9 3/8
+		9.5, // 9 1/2
+		9.625, // 9 5/8
+		9.75, // 9 3/4
+		9.875, // 9 7/8
+		10, // 10
+		10.125, // 10 1/8
+		10.25, // 10 1/4
+		10.5, // 10 1/2
+		10.75, // 10 3/4
+		11, // 11
+		11.25 // 11 1/4
 	]
 };
 
@@ -46,6 +46,8 @@ const defaultSizes = {
 	in: {m: unisex.in, w: unisex.in}
 };
 
+const types = ['w', 'm', 'k'];
+
 const isString = value => {
 	return typeof value === 'string' || Object.prototype.toString.call(value) === '[object String]';
 };
@@ -55,7 +57,7 @@ const hasOwnProps = (object, key) => {
 };
 
 const isValidType = type => {
-	return type === 'm' || type === 'w';
+	return types.includes(type);
 };
 
 const isValidCountry = country => {
@@ -80,11 +82,116 @@ const isValidOutput = outputs => {
 	return hasOwnProps(defaultSizes, outputs[0]);
 };
 
+const partNumberToNumber = partNumber => {
+	const parts = partNumber.split('/');
+
+	return parts[0] / parts[1];
+};
+
+const inchesStringToNumber = inchesString => {
+	const parts = inchesString.split(' ');
+
+	if (parts.length === 1) {
+		return Number(parts[0]);
+	}
+
+	return Number.parseInt(parts[0], 10) + (parts[1] ? partNumberToNumber(parts[1]) : 0);
+};
+
+const inchesNumberToString = inchesNumber => {
+	// ToFixed is used in order to avoid JS aritchmetic issues
+	const rest = Number((inchesNumber % 1).toFixed(5));
+
+	if (!rest) {
+		return String(inchesNumber);
+	}
+
+	let denominator = 8;
+	let numerator = rest * denominator;
+
+	if (Number((numerator % 1).toFixed(5)) !== 0) {
+		return String(inchesNumber);
+	}
+
+	while (denominator > 1 && Number((numerator % 2).toFixed(1)) === 0) {
+		denominator /= 2;
+		numerator /= 2;
+	}
+
+	return `${Number.parseInt(inchesNumber, 10)} ${numerator}/${denominator}`;
+};
+
+const compare = (a, b, smaller = true) => {
+	if (smaller) {
+		return a <= b;
+	}
+
+	return a > b;
+};
+
+const getClosestValidSize = (country, inTypes, inSize, small = true) => {
+	if (isString(inTypes)) {
+		inTypes = [inTypes];
+	}
+
+	for (const type of inTypes) {
+		if (!isValidType(type)) {
+			throw new Error(`${type} is not supported as a type.`);
+		}
+	}
+
+	if (!isValidCountry(country)) {
+		throw new Error(`${country} is not supported as a country.`);
+	}
+
+	const orderedTypes = [...types.filter(type => inTypes.includes(type))];
+
+	if (small) {
+		orderedTypes.reverse();
+	}
+
+	let current = null;
+
+	for (const type of orderedTypes) {
+		if (!defaultSizes[country][type]) {
+			continue;
+		}
+
+		const sizes = [...defaultSizes[country][type]];
+
+		if (!small) {
+			sizes.reverse();
+		}
+
+		if (current === null) {
+			current = {type, size: sizes[0]};
+
+			if (compare(inSize, sizes[0], small)) {
+				return current;
+			}
+		}
+
+		for (const size of sizes) {
+			if (compare(inSize, size, small)) {
+				if (compare(size - inSize, inSize - current.size, !small)) {
+					return current;
+				}
+
+				return {type, size};
+			}
+
+			current = {type, size};
+		}
+	}
+
+	return current;
+};
+
 function converter(country, type, size, out = ['eu', 'br', 'cm', 'in']) {
 	const output = isString(out) ? [out] : out;
 
 	if (!isValidType(type)) {
-		type = 'm';
+		type = types[0];
 	}
 
 	if (!isValidCountry(country)) {
@@ -104,7 +211,11 @@ function converter(country, type, size, out = ['eu', 'br', 'cm', 'in']) {
 
 	const converteds = {};
 	for (const key of output) {
-		const convertedValue = defaultSizes[key][type][position];
+		let convertedValue = defaultSizes[key][type][position];
+
+		if (key === 'in') {
+			convertedValue = inchesNumberToString(convertedValue);
+		}
 
 		converteds[key] = convertedValue;
 	}
@@ -112,5 +223,60 @@ function converter(country, type, size, out = ['eu', 'br', 'cm', 'in']) {
 	return converteds;
 }
 
+function convertSizeRange(country, inTypes, inSizes, inOutput = ['eu', 'br', 'cm', 'in']) {
+	const output = isString(inOutput) ? [inOutput] : inOutput;
+
+	if (isString(inTypes)) {
+		inTypes = [inTypes];
+	}
+
+	for (const type of inTypes) {
+		if (!isValidType(type)) {
+			throw new Error(`${type} is not supported as a type.`);
+		}
+	}
+
+	if (!isValidCountry(country)) {
+		throw new Error(`${country} is not supported as a country.`);
+	}
+
+	if (!isValidOutput(output)) {
+		throw new Error(`${output} is not a valid output.`);
+	}
+
+	if (!Array.isArray(inSizes) || inSizes.length !== 2) {
+		throw new Error(`${inSizes} is not a two-value array.`);
+	}
+
+	let smallSize = inSizes[0];
+	let largeSize = inSizes[1];
+
+	// If inches, ensure we deal with numbers
+	if (country.includes('in')) {
+		if (isString(smallSize)) {
+			smallSize = inchesStringToNumber(smallSize);
+		}
+
+		if (isString(largeSize)) {
+			largeSize = inchesStringToNumber(largeSize);
+		}
+	}
+
+	const small = getClosestValidSize(country, inTypes, smallSize);
+	const large = getClosestValidSize(country, inTypes, largeSize, false);
+
+	return [
+		{sizes: converter(country, small.type, small.size, output), type: small.type},
+		{sizes: converter(country, large.type, large.size, output), type: large.type}
+	];
+}
+
 export default converter;
-export {converter, defaultSizes as sizes};
+export {
+	converter,
+	convertSizeRange,
+	getClosestValidSize,
+	inchesStringToNumber,
+	inchesNumberToString,
+	defaultSizes as sizes
+};
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import {converter, sizes} from './index.js'; // eslint-disable-line unicorn/import-index, import/no-useless-path-segments, import/extensions
+import {converter, convertSizeRange, getClosestValidSize, inchesStringToNumber, inchesNumberToString, sizes} from './index.js'; // eslint-disable-line unicorn/import-index, import/no-useless-path-segments, import/extensions
 
 test('ensure that a random size have the same length of the other', t => {
 	const euSize = sizes.eu.w.length;
@@ -72,3 +72,163 @@ test('verify string output', t => {
 	t.is(sizes.br, 37);
 });
 
+const inchesNumberStringPairs = [
+	[3, '3'],
+	[3.125, '3 1/8'],
+	[3.25, '3 1/4'],
+	[3.375, '3 3/8'],
+	[3.5, '3 1/2'],
+	[3.4321, '3.4321']
+];
+
+function inchesStringToNumberMacro(t, input, expected) {
+	t.is(inchesStringToNumber(input), expected);
+}
+
+for (const [number, string] of inchesNumberStringPairs) {
+	test(`inchesStringToNumber: ${string} => ${number}`, inchesStringToNumberMacro, string, number);
+}
+
+function inchesNumberToStringMacro(t, input, expected) {
+	t.is(inchesNumberToString(input), expected);
+}
+
+for (const [number, string] of inchesNumberStringPairs) {
+	test(`inchesNumberToString: ${number} => ${string}`, inchesNumberToStringMacro, number, string);
+}
+
+test('getClosestValidSize: Size smaller than smallest available', t => {
+	const size = getClosestValidSize('eu', 'm', 6);
+
+	t.deepEqual(size, {type: 'm', size: sizes.eu.m[0]});
+});
+
+test('getClosestValidSize: Size larger than largest available', t => {
+	const size = getClosestValidSize('eu', 'm', 47);
+
+	t.deepEqual(size, {type: 'm', size: sizes.eu.m[sizes.eu.m.length - 1]});
+});
+
+test('getClosestValidSize: Size smaller than smallest available - reverse', t => {
+	const size = getClosestValidSize('eu', 'm', 6, false);
+
+	t.deepEqual(size, {type: 'm', size: sizes.eu.m[0]});
+});
+
+test('getClosestValidSize: Size larger than largest available - reverse order', t => {
+	const size = getClosestValidSize('eu', 'm', 47, false);
+
+	t.deepEqual(size, {type: 'm', size: sizes.eu.m[sizes.eu.m.length - 1]});
+});
+
+test('getClosestValidSize: Find size in first set - rounded down', t => {
+	const size = getClosestValidSize('us', ['m', 'w'], 6.125);
+
+	t.deepEqual(size, {type: 'm', size: 6});
+});
+
+test('getClosestValidSize: Find size in first set - rounded up', t => {
+	const size = getClosestValidSize('us', ['m', 'w'], 6.375);
+
+	t.deepEqual(size, {type: 'm', size: 6.5});
+});
+
+test('getClosestValidSize: Find size in second set', t => {
+	const size = getClosestValidSize('us', ['m', 'w'], 13.125);
+
+	t.deepEqual(size, {type: 'w', size: 13});
+});
+
+test('getClosestValidSize: Find size in first set - rounded down - reverse', t => {
+	const size = getClosestValidSize('us', ['m', 'w'], 6.125, false);
+
+	t.deepEqual(size, {type: 'w', size: 6});
+});
+
+test('getClosestValidSize: Find size in first set - rounded up - reverse', t => {
+	const size = getClosestValidSize('us', ['m', 'w'], 6.375, false);
+
+	t.deepEqual(size, {type: 'w', size: 6.5});
+});
+
+test('getClosestValidSize: Find size in second set - reverse', t => {
+	const size = getClosestValidSize('us', ['m', 'w'], 3.625, false);
+
+	t.deepEqual(size, {type: 'm', size: 3.5});
+});
+
+test('getClosestValidSize: Find size straight in between', t => {
+	const size = getClosestValidSize('ca', ['m', 'k', 'w'], 12.25);
+
+	t.deepEqual(size, {type: 'm', size: 12.5});
+});
+
+test('getClosestValidSize: Find size straight in between - reverse', t => {
+	const size = getClosestValidSize('ca', ['m', 'k', 'w'], 10.5, false);
+
+	t.deepEqual(size, {type: 'w', size: 11});
+});
+
+test('convertSizeRange: EU m/w 37-46', t => {
+	const size = convertSizeRange('eu', ['m', 'w'], [37, 46]);
+
+	t.deepEqual(size, [{
+		sizes: {
+			br: 35,
+			cm: 24,
+			eu: 37,
+			in: '9 3/8'
+		},
+		type: 'm'
+	}, {
+		sizes: {
+			br: 45,
+			cm: 28.6,
+			eu: 46.5,
+			in: '11 1/4'
+		},
+		type: 'w'
+	}]);
+});
+
+test('convertSizeRange: IN m/k/w 9 5/8 - 10.25', t => {
+	const size = convertSizeRange('in', ['m', 'k', 'w'], ['9 5/8', 10.25]);
+
+	t.deepEqual(size, [{
+		sizes: {
+			br: 36,
+			cm: 25,
+			eu: 38,
+			in: '9 5/8'
+		},
+		type: 'm'
+	}, {
+		sizes: {
+			br: 40,
+			cm: 26,
+			eu: 42,
+			in: '10 1/4'
+		},
+		type: 'w'
+	}]);
+});
+
+test('convertSizeRange: ca m/k/w 4.875 - 10.5', t => {
+	const size = convertSizeRange('ca', ['m', 'k', 'w'], [4.875, 10.5], ['us', 'uk', 'eu']);
+
+	t.deepEqual(size, [{
+		sizes: {
+			us: 5,
+			uk: 4.5,
+			eu: 37
+		},
+		type: 'm'
+	}, {
+		sizes: {
+			us: 11,
+			uk: 9,
+			eu: 43
+		},
+		type: 'w'
+	}]);
+});
